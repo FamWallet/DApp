9aa04bdc3d97a2daece8888ebd3183f771c90411
benjamh <ben.t.hsieh@gmail.com>
2023-03-19 15:50:25 -0700

    final push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,11 @@ import styles from "@/styles/Home.module.css";
 import { GoogleLogin } from "@react-oauth/google";
 import { useGoogleOneTapLogin } from "@react-oauth/google";
 import { googleLogout } from '@react-oauth/google';
+import { redirect } from 'next/navigation';
+import { hasGrantedAllScopesGoogle } from '@react-oauth/google';
+
+
+
 
 
 // const login = useGoogleOneTapLogin({
@@ -28,18 +33,29 @@ export default function Home() {
 			</Head>
 			<main className={styles.main}>
         <header>
-          FamWallet
+          <img src="/FamWalletLogo.svg"></img>
         </header>
+		<section className={styles.taCenter}>
+		<img src="/btcWallet.png"></img>
+		</section>
+
+		<section className={styles.taCenter + " " + styles.jingleTextOne}>
+			From Fan to Fam
+			<div className={styles.jingleTextTwo}>Quickest way to onboard and engage with your fans.</div>
+		</section>
 				<GoogleLogin
 					onSuccess={(credentialResponse) => {
-						console.log(credentialResponse);
+						redirect("/Profile")
 					}}
 					onError={() => {
 						console.log("Login Failed");
 					}}
 				/>
-				;
+					<footer className={styles.taCenter + " " + styles.footerText}>
+			By connecting your wallet you agree to our <span className={styles.white}>Terms of Service</span> and <span className={styles.white}>Privacy Policy</span>
+			</footer>
 			</main>
+		
 		</>
 	);
 }
